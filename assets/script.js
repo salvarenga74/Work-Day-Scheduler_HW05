ec096283c911ce6c68918d68c9cdfffeae8ade18
Sally Alvarenga <sallyalvarenga35@gmail.com>
2021-08-17 01:02:02 -0500

    Second session with tutor to complete a more efficient way of saving to local storage. Still does not work,but at least im closer than i was before ( in theory).

--- assets/script.js
+++ assets/script.js
@@ -1,7 +1,8 @@
 var today = moment();
 var currentHour = moment().hour();
 var message = document.querySelector('.popUpComments');
-var saveBtn = document.querySelector('.saveBtn');
+var saveBtn = document.querySelector('.container');
+var inputSpan = document.querySelector('.description');
 
 // formats the current day for the page header
 $('#currentDay').text(today.format("dddd, MMMM Do, YYYY"));
@@ -9,7 +10,7 @@ $('#currentDay').text(today.format("dddd, MMMM Do, YYYY"));
 // Runs program to color code the workday according to current hour as soon as page loads.
 whatHourIsIt();
 renderPreviousSavedInfo();
-console.log(currentHour);
+console.log("Current Hour: " + currentHour);
 
 saveBtn.addEventListener("click", saveAllInputs);
 
@@ -34,18 +35,56 @@ function whatHourIsIt(){
 }
 
 function renderPreviousSavedInfo(){
-    var userInputs = localStorage.getItem(".description");
-    
-    
+    var userInputs = localStorage.getItem("descriptions");
+    if (!userInputs){
+        return;
+    }
+    userInputs = JSON.parse(userInputs);
+        for (let i = 0; i < userInputs.length; i++ ){
+            var currentHour2 = userInput[i];
+            var hour = Object.keys(currentHour2)[0]
+            var targetBox = document.querySelector(`#hr${hour}`)
+            targetBox.textContent = currentHour2[hour];
+        }
 }
 
-function saveAllInputs(){
-    message.textContent = ("Successfully Saved ✅")
-    message.classList.add("successfullySaved");
-    var 
+function saveAllInputs(event){
+    var element = event.target;
 
+    // specifies that we only care if the click is on a blue save button or the icon.
+    if (element.matches(".saveBtn")||(element.matches(".fas"))) {
+        message.textContent = ("Successfully Saved ✅")
+        message.classList.add("successfullySaved");
+
+        var storedInputArray=[];
+        var inputs = document.querySelectorAll('.description');
+        for (let i = 0; i < inputs.length; i++) {
+            const inputObj = inputs[i];
+            const currentHour = i+9;
+            console.log("input text: " + inputObj.value);
+            const newObject ={
+                // cuz this is an object, the [] specifies the value of the variable, as opposed to the name
+                [currentHour]:inputObj.value
+            };
+            console.log("new array object" + newObject);
+            storedInputArray.push(newObject);
+        }
+        console.log("storedInputArray: "+ storedInputArray);
+        var localDescriptions = localStorage.getItem("descriptions");
+        
+        // here we turn the string into an array
+        localDescriptions = JSON.parse(localDescriptions);
+
+        // here we push more items into said array. 
+        localDescriptions.push(...storedInputArray)
+
+        // spit it back to string for storage.
+        localStorage.setItem("descriptions", JSON.stringify(localDescriptions));
+
+        renderPreviousSavedInfo();
+    }
 }
-
+//  class activity 26 is about local storage too. 
 
 
 
